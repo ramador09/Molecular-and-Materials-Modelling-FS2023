49d711b21775eff38d12a835345aa5c88b214615
Raymond C Amador <106821034+ramador09@users.noreply.github.com>
2023-04-13 21:13:28 +0200

    uploaded PBE/SZV

---
+++ exercise7/exercise-7_outputfiles/PBE/SZV/ts_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#
+#
+#SBATCH --job-name="PBE_DZVP_ts"
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --partition=normal
+#SBATCH --ntasks-per-node=12
+#SBATCH --cpus-per-task=1
+#SBATCH --constraint=gpu
+#========================================
+# load modules and run simulation
+
+module load daint-gpu
+module load CP2K
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
+export CRAY_CUDA_MPS=1
+ulimit -s unlimited
+
+srun cp2k.psmp -i ts.inp -o ts.out
